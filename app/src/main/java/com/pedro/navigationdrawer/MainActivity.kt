98974dc854707cb8a9c693de55592a3a7d36a84d
20241TIIMI0097 <souzapedroalmeida8@gmail.com>
2026-09-18 09:57:48 -0300

    Atividade completa e funcional

--- app/src/main/java/com/pedro/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/pedro/navigationdrawer/MainActivity.kt
@@ -2,6 +2,8 @@ package com.pedro.navigationdrawer
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -37,9 +39,35 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         // supportActionBar?.title = ""
         binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_cart -> openFragment(CartFragment())
+                R.id.bottom_profile -> openFragment(ProfileFragment())
+                R.id.bottom_menu -> openFragment(MenuFragment())
+            }
+            true
+        }
+
+        fragmentManager = supportFragmentManager
+        openFragment(HomeFragment())
+
+        binding.fab.setOnClickListener {
+            Toast.makeText(this, "Categorias", Toast.LENGTH_SHORT).show()
+        }
+
+        onBackPressedDispatcher.addCallback(this){
+            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                finish()
+            }
+        }
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+    override fun onNavigationItemSelected (item: MenuItem): Boolean {
         when(item.itemId){
             R.id.nav_prime -> openFragment(PrimeFragment())
             R.id.nav_carros -> openFragment(CarsFragment())
